ce6c2c940a9e81542e22e765b40526916e1988c9
Mikalai Lukyanchyk <Mikalai.Lukyanchyk@gmail.com>
2023-03-22 18:32:27 +0000

    test image

--- test_kuber/test.sh
+++ test_kuber/test.sh
@@ -5,6 +5,7 @@ TOKEN=A537ZEWZLU2QR2JLFFXE6ADEDNLA6
 
 cd /actions-runner
 
+export RUNNER_ALLOW_RUNASROOT="1"
 
 ./config.sh --unattended --replace --url https://github.com/${REPO} --token ${TOKEN}
 
